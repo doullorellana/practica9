df9d6b808c75bc55ab32d4da7c0dacba700eccdd
doullorellana <doullorellana@gmail.com>
2023-10-17 21:02:37 -0600

    ya mostrando cards con imagenes en la pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,25 @@ function App() {
     fetch(URL)
       .then((res) => res.json())
       .then((datos) => {
-        setData(datos.result)
-        console.log(datos);
-
+        setData(datos.results);
+        console.log(datos.results);
       });
   }, []);
 
   return (
-    <>
-    </>
+    <main>
+      {data.map((el) => (
+        <div key={el.id} className="card">
+          <div className="divImg">
+            <img src={el.image} alt={el.name} />
+          </div>
+          <div className="divText">
+            <h2>{el.name}</h2>
+            <p>Especie: {el.species}</p>
+          </div>
+        </div>
+      ))}
+    </main>
   );
 }
 
